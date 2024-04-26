24b510f84a9b099827c42d7ea61f12f043cbdf18
Sıla Mahim <silamahim@Sla-MacBook-Air.local>
2024-04-26 13:32:38 +0300

    created router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import QuestionPage from "./pages/QuestionPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-center align">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" index element={<HomePage />} />
+          <Route path="/question" element={<QuestionPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../components/Card";
+
+function HomePage() {
+  return (
+    <>
+      <Card />
+    </>
+  );
+}
+
+export default HomePage;
